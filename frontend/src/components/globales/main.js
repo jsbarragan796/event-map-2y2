6e7d0306c987daf8f3fcaca4165bba38b03b86b7
Santiago Cortes <s.cortes@uniandes.edu.co>
2018-03-04 21:26:34 -0500

    routers funcionales

--- frontend/src/components/globales/main.js
+++ frontend/src/components/globales/main.js
@@ -18,17 +18,17 @@ export class Main extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={About} />
-        <Route path="/signup" component={SingupForm} />
-        <Route path="/login" component={LoginForm} />
+        <Route exact path="/" component={About} />
+        <Route exact path="/signup" component={SingupForm} />
+        <Route exact path="/login" component={LoginForm} />
 
-        <Route path="/user/:idUser" component={CommonUser} />
-        <Route path="/enterprice/:idEnterprice" component={EnterpriceUser} />
+        <Route path="/user" component={CommonUser} />
+        <Route path="/enterprice" component={EnterpriceUser} />
 
-        <Route path="/locations" component={LocationList} />
-        <Route path="/locations/:idLocation" component={LocationDetail} />
-        <Route path="/events" component={EventList} />
-        <Route path="/events/:idEvent" component={EventDetail} />
+        <Route exact path="/locations" component={LocationList} />
+        
+        <Route exact path="/events" component={EventList} />
+        
 
       </Switch>
     );
--- frontend/src/components/globales/navHeader.js
+++ frontend/src/components/globales/navHeader.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-import { Col, Row, Grid, Clearfix, Navbar, Nav, NavItem } from "react-bootstrap";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 
 
 export class NavHeader extends Component {
   render() {
-    return(
-      <Grid >
-        <p>header</p>
-      </Grid>
+    return (
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#brand">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} componentClass={Link} href="/LocationList" to="/LocationList">
+              Lugares a donde ir!
+            </NavItem>
+            <NavItem eventKey={1} componentClass={Link} href="/EventList" to="/EventList">            
+              Eventos cercanos!
+            </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} componentClass={Link} href="/login" to="/login">
+              Login!
+            </NavItem>
+            <NavItem eventKey={1} componentClass={Link} href="/signup" to="/signup">
+              Signup!
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
